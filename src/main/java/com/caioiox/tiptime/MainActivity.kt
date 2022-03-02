5d7f222c3e1dadab39c8c5cefe2045fc50de4763
Caio <99915224+CaioIOX@users.noreply.github.com>
2022-03-01 21:04:59 -0300

    Add files via upload

---
+++ src/main/java/com/caioiox/tiptime/MainActivity.kt
@@ -0,0 +1,62 @@
+package com.caioiox.tiptime
+
+import android.content.Context
+import android.os.Bundle
+import android.view.KeyEvent
+import android.view.View
+import android.view.inputmethod.InputMethodManager
+import androidx.appcompat.app.AppCompatActivity
+import com.caioiox.tiptime.databinding.ActivityMainBinding
+import java.text.NumberFormat.getCurrencyInstance
+
+class MainActivity : AppCompatActivity(){
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.calculateButton.setOnClickListener{calculateTip()}
+
+        binding.costOfServiceEditText.setOnKeyListener {view, keyCode, _ -> handleKeyEvent(view, keyCode) }
+    }
+    private fun displayTip(tip: Double){
+        val formattedTip = getCurrencyInstance().format(tip)
+        binding.tipResult.text = getString(R.string.gorjeta, formattedTip)
+    }
+    private fun calculateTip(){
+
+        val stringInTextField = binding.costOfServiceEditText.text.toString()
+        val cost = stringInTextField.toDoubleOrNull()
+
+        if (cost==null || cost==0.0){
+            displayTip(0.0)
+            return
+        }
+
+        val tipPercentage = when (binding.tipOptions.checkedRadioButtonId){
+            R.id.option_twenty_percent -> 0.20
+            R.id.option_eighteen_percent -> 0.18
+            else -> 0.15
+        }
+
+        var tip = cost * tipPercentage
+
+        if (binding.roundUpSwitch.isChecked){
+            tip = kotlin.math.ceil(tip)
+        }
+        displayTip(tip)
+
+    }
+    private fun handleKeyEvent (view: View, keyCode: Int): Boolean{
+        if (keyCode == KeyEvent.KEYCODE_ENTER){
+            val inputMethodManager =
+                getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+            inputMethodManager.hideSoftInputFromWindow(view.windowToken, 0)
+            return true
+        }
+        return false
+    }
+}
